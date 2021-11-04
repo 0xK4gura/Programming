a919f6f2719060e7af6a327f342a86a5e3462c28
0xK4gura <kaguramaru11@gmail.com>
2021-11-05 02:26:02 +0800

    Added simple C program

---
+++ C/#1 name.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("\t\tKagura Class\n\n");
+
+	char str[50]; // char array of size 50
+ 	printf("Enter your full name:\n\n\n");
+ 	
+ 	gets(str);
+ 	printf("\nWelcome %s\n\n\n", str);
+ 	
+	printf("\n\n\t\tKagura Class Lets Go!");
+	
+    return 0;
+}
