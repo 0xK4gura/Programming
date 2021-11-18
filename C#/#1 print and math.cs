9074e45494041413cfdb9216d8877b8735f26737
0xK4gura <kaguramaru11@gmail.com>
2021-11-18 09:40:33 +0800

    Added C\#

---
+++ C#/#1 print and math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! In this app we will just create a simple print and mathematic program");
+            int a = 1;
+            int b = 1;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
